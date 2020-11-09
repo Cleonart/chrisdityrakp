e8d9724ee78ba51a4aacc89cc967d2dd7abe9fec
Chrisdityra Lengkey <61264981+chlengkey@users.noreply.github.com>
2020-11-09 23:10:28 +0800

    11092020 - Rev 1

--- app/src/views/artikel/tambahArtikel.vue
+++ app/src/views/artikel/tambahArtikel.vue
@@ -1,6 +1,16 @@
 <template>
 	<div>
 		<h3>Tambah Artikel</h3>
+		<b-form-group	id="input-group-1"
+        				label="Nama Jurnal"
+        				label-for="input-1"
+        				description="Silahkan memasukkan nama jurnal usulan">
+        	<b-form-input 	id="input-1"
+        				    type="text"
+          					required
+          					placeholder="Cth. RealTech"></b-form-input>
+        </b-form-group>
+
 		<file-pond
         	name="test"
         	ref="pond"
